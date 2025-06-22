55a85ad28c700df6ef046bcee48c17d2c878b4a1
ReCalles <129238447+ReCalles@users.noreply.github.com>
2025-06-22 14:15:53 -0600

    Added Python Files

---
+++ days_1_14/day_2/01_data_types/data_types.py
@@ -0,0 +1,18 @@
+# Subscripting
+print ("Hello"[4])
+
+# String
+print("123" + "345")
+
+#Integer = Whole Numbers
+print(123 + 345)
+
+# Large Numbers usually divided by comas are divided in Python by underscore
+print(123_345_567)
+
+# Float = Floating Point Number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
---
+++ days_1_14/day_2/03_mathematical_operations/mathematical_operations.py
@@ -0,0 +1,32 @@
+# ## Basic Operators
+
+# Learn to use the basic mathematical operators, +, -, *, /, // and **
+
+# ## PEMDAS
+# Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+
+# Sum
+print(123 + 456)
+# Rest
+print(7 - 3)
+# Multiplication
+print(3 * 2)
+# Division
+print(5 / 3)
+# Division Rounding up number
+print(5 // 3)
+# Exponent
+print(2 ** 3)
+
+
+# ### PAUSE 1. What is the output of this code? Answer: 7.0
+print(3 * 3 + 3 / 3 - 3)
+
+
+
+# ### PAUSE 2. Change the code so it outputs 3?
+# `print(3 * 3 + 3 / 3 - 3)`
+
+print(3 * (3 + 3) / 3 - 3)
+
+
