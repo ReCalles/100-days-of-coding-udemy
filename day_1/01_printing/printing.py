368b5792fcfc8ea7c97c841ceb98bfee597b8729
ReCalles <129238447+ReCalles@users.noreply.github.com>
2025-06-22 13:29:05 -0600

    Added Python files to day 1

---
+++ day_1/01_printing/printing.py
@@ -0,0 +1,10 @@
+# Use what you learnt to print out the words "Hello world!" with Python code.
+# 
+# Then click the run button to execute your code.
+# 
+#  Hint 
+# Remember that this is the format to print things in Python: print("some text")
+
+
+# Write your code below this line 👇
+print("Hello World")
---
+++ day_1/02_string_manipulation/string_manipulation.py
@@ -0,0 +1,38 @@
+# Learn to use string concatenation and the new line escape sequence to format strings in Python.
+#
+# PAUSE 1. Use \n to add another line of "Hello world".
+# So the resulting output looks like this:
+#
+# Hello world!
+#
+# Hello world!
+#
+# Hello world!
+
+# 1. New Line Escape Sequence
+
+# Learn to use `\n` to add a new line to strings.
+
+print("Hello")
+print("Word")
+
+# is the same as:
+
+print("Hello\nWorld")
+
+
+# PAUSE 2. Add a space between the strings
+# So there is a space between the string Hello and Angela when the print statement runs.
+#
+# The output should look like this:
+#
+# Hello Angela
+
+print("Hello Angela")
+
+# 2. String Concatenation
+# Use the plus symbol `+` to concatenate different strings together
+
+print("My name is" + " " + "Angela")
+
+# will output: "My name is Angela"
